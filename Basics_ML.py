342359bef2794a0b4d54bc9ff723a7bd77417038
raja-spec <rraja48569@gmail.com>
2023-08-05 17:26:32 +0530

    first commit

---
+++ Basics_ML.py
@@ -0,0 +1,12 @@
+#      INTRO TO MACHINE LEARNING
+''' 
+ * It is a technique which learns from the given 
+   data, that is mainly based on training a dataset
+
+ * It also includes the concepts of data science
+   which helps to analysis the given data after
+   analyze the data that data are given to the 
+   machine.
+
+ 
+'''
