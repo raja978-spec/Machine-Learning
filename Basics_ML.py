d64b8bb6ececa47b7e7fdf42a2c945e81ca147e2
raja-spec <rraja48569@gmail.com>
2023-08-05 17:42:43 +0530

    Type of machine learning

--- Basics_ML.py
+++ Basics_ML.py
@@ -1,4 +1,4 @@
-#      INTRO TO MACHINE LEARNING
+#               INTRO TO MACHINE LEARNING
 ''' 
  * It is a technique which learns from the given 
    data, that is mainly based on training a dataset
@@ -8,5 +8,38 @@
    analyze the data that data are given to the 
    machine.
 
- 
+* It uses the concept called features and
+  labels to construct a dataset. 
+
+  Example for features: Human body's features
+                        
+                        1. Eye
+                        2. Ear
+                        3. Two leg, hand with ten fingures
+                        
+
+  Example for labels: An object with these features are
+                      called Human, "Human" that is called
+                      labels.
+                
+'''
+
+#              TYPES OF MACHINE LEARNING
+'''
+ 1. Supervised Learning   - Here we give features with 
+                            labeled data, through that 
+                            data it can learning
+         
+ 2. UnSupervised Learning - It doesn't have labeled data
+                            instead that it has only 
+                            features
+
+                            EX: It finds similar pattern
+                            from unknow data, and tries
+                            to find what type of data is
+                            that
+
+                            EX2: youtube recommandation 
+                                 system.
+ 3. Reinforcement Learning
 '''
