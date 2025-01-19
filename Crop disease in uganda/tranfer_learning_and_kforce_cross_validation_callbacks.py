6822a4089110f895742e4063068fa6d133e33e15
raja <rraja48569@gmail.com>
2025-01-19 12:08:34 +0530

    callback functions implementation and plotting the results

--- Crop disease in uganda/tranfer_learning_and_kforce_cross_validation_callbacks.py
+++ Crop disease in uganda/tranfer_learning_and_kforce_cross_validation_callbacks.py
@@ -211,7 +211,7 @@
                               lr_scheduler(freq, decrease_rate)
 '''
 
-#              LEARNING RATE SCHEDULER
+#              LEARNING RATE SCHEDULER FUNCTION
 '''
  For the Learning Rate Scheduling, we'll use StepLR 
  from torch.optim. The StepLR scheduler decays the 
@@ -250,4 +250,252 @@
  ) 
 
  print(type(scheduler))
+'''
+
+#               EARLY STOPPING FUNCTION
+'''
+ For Early Stopping, we'll create a function early_stopping 
+ that we'll call from within the train function. 
+ 
+ The early_stopping function accepts:
+
+ the current validation loss,
+ the best validation loss so far
+ the number of epochs since validation loss last improved (counter).
+ In the function we need to check if validation loss improved. 
+ If yes, we reset the counter. If not, we add one to the counter. 
+ We also need to check if validation loss hasn't improved in the 
+ last 5 epochs. If that is the case, we should set stopping to True.
+
+ def early_stopping(validation_loss, best_val_loss, counter):
+    """Function that implements Early Stopping"""
+
+    stop = False
+
+    if validation_loss < best_val_loss:
+        counter = 0
+    else:
+        counter += 1
+
+    # Check if counter is >= patience (5 epochs in our case)
+    # Set stop variable accordingly
+    if counter >= 5:
+        stop = True
+
+    return counter, stop
+'''
+
+#              MODEL CHECKPOINTING FUNCTION
+'''
+ def checkpointing(validation_loss, best_val_loss, model, optimizer, save_path):
+
+    if validation_loss < best_val_loss:
+        torch.save(
+            {
+                "model_state_dict": model.state_dict(),
+                "optimizer_state_dict": optimizer.state_dict(),
+                "loss": best_val_loss,
+            },
+            save_path,
+        )
+        print(f"Checkpoint saved with validation loss {validation_loss:.4f}")
+'''
+
+#   USING CALLBACKS IN MODEL TRAINING
+'''
+ Now we're ready to modify the train function to include an 
+ option to use Callbacks.
+
+ Notice that the modified train function below is 
+ quite similar to what we've used before. We just added scheduler, 
+ checkpoint_path and early_stopping as optional arguments. 
+ 
+ As you can see at the end of the modified train function, 
+ we use these three callbacks when function is called with appropriate inputs.
+
+ from training import score, train_epoch
+
+
+ def train(
+    model,
+    optimizer,
+    loss_fn,
+    train_loader,
+    val_loader,
+    epochs=20,
+    device="cpu",
+    scheduler=None,
+    checkpoint_path=None,
+    early_stopping=None,
+):
+    # Track the model progress over epochs
+    train_losses = []
+    train_accuracies = []
+    val_losses = []
+    val_accuracies = []
+    learning_rates = []
+
+    # Create the trackers if needed for checkpointing and early stopping
+    best_val_loss = float("inf")
+    early_stopping_counter = 0
+
+    print("Model evaluation before start of training...")
+    # Test on training set
+    train_loss, train_accuracy = score(model, train_loader, loss_fn, device)
+    train_losses.append(train_loss)
+    train_accuracies.append(train_accuracy)
+    # Test on validation set
+    validation_loss, validation_accuracy = score(model, val_loader, loss_fn, device)
+    val_losses.append(validation_loss)
+    val_accuracies.append(validation_accuracy)
+
+    for epoch in range(1, epochs + 1):
+        print("\n")
+        print(f"Starting epoch {epoch}/{epochs}")
+
+        # Train one epoch
+        train_epoch(model, optimizer, loss_fn, train_loader, device)
+
+        # Evaluate training results
+        train_loss, train_accuracy = score(model, train_loader, loss_fn, device)
+        train_losses.append(train_loss)
+        train_accuracies.append(train_accuracy)
+
+        # Test on validation set
+        validation_loss, validation_accuracy = score(model, val_loader, loss_fn, device)
+        val_losses.append(validation_loss)
+        val_accuracies.append(validation_accuracy)
+
+        print(f"Epoch: {epoch}")
+        print(f"Training loss: {train_loss:.4f}")
+        print(f"Training accuracy: {train_accuracy*100:.4f}%")
+        print(f"Validation loss: {validation_loss:.4f}")
+        print(f"Validation accuracy: {validation_accuracy*100:.4f}%")
+
+        # # Log the learning rate and have the scheduler adjust it
+        lr = optimizer.param_groups[0]["lr"]
+        learning_rates.append(lr)
+        if scheduler:
+            scheduler.step()
+
+        # Checkpointing saves the model if current model is better than best so far
+        if checkpoint_path:
+            checkpointing(
+                validation_loss, best_val_loss, model, optimizer, checkpoint_path
+            )
+
+        # Early Stopping
+        if early_stopping:
+            early_stopping_counter, stop = early_stopping(
+                validation_loss, best_val_loss, early_stopping_counter
+            )
+            if stop:
+                print(f"Early stopping triggered after {epoch} epochs")
+                break
+
+        if validation_loss < best_val_loss:
+            best_val_loss = validation_loss
+
+    return (
+        learning_rates,
+        train_losses,
+        val_losses,
+        train_accuracies,
+        val_accuracies,
+        epoch,
+    )
+
+    epochs_to_train = 50
+
+train_results = train(
+    model,
+    optimizer,
+    loss_fn,
+    train_loader,
+    val_loader,
+    epochs=epochs_to_train,
+    device=device,
+    scheduler=scheduler,
+    checkpoint_path="model/LR_model.pth",
+    early_stopping=early_stopping,
+)
+
+(
+    learning_rates,
+    train_losses,
+    valid_losses,
+    train_accuracies,
+    valid_accuracies,
+    epochs,
+) = train_results
+'''
+
+# PLOTTING ALL THE RESULTS AND CONFUSION MATRIX
+'''
+ The above function will be stopped early so it trains only 15 epochs.
+
+ In below we will plot how train and val loss and accuracy changes over epochs.
+
+ # Plot train losses, use label="Training Loss"
+
+ plt.plot(train_losses, label="Training Loss")
+ plt.plot(valid_losses, label="Validation Loss")
+ plt.ylim([0, 1.7])
+ plt.title("Loss over epochs")
+ plt.xlabel("Epochs")
+ plt.ylabel("Loss")
+ plt.legend();
+
+ In above chart output the loss function are decreased
+ as expected.
+
+ # Plot train accuracies, use label="Training Accuracy"
+ plt.plot(train_accuracies, label='Training Accuray')
+ # Plot validation accuracies, use label="Validation Accuracy"
+ plt.plot(valid_accuracies, label="Validation Accuray")
+ plt.ylim([0, 1])
+ plt.title("Accuracy over epochs")
+ plt.xlabel("Epochs")
+ plt.ylabel("Accuracy")
+ plt.legend();
+
+ In the above chart the accuracy is also increased as expected.
+
+ # Plot the learning rates
+ plt.figure(figsize=(10, 6))
+ plt.plot(range(1, epochs + 1), learning_rates, marker="o", label="Learning Rate")
+ plt.title("Learning Rate Schedule")
+ plt.xlabel("Epoch")
+ plt.ylabel("Learning Rate")
+ plt.show()
+
+ In the above chart the learning rate is decreased as expected.
+
+ Now we will see the saved models by checkpointing.
+
+ checkpoint = torch.load("model/LR_model.pth")
+
+ # Load the state dictionaries
+ model.load_state_dict(checkpoint["model_state_dict"])
+ optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
+
+ from training import predict
+
+ probabilities_val = predict(model, val_loader, device)
+ predictions_val = torch.argmax(probabilities_val, dim=1)
+
+ print(predictions_val)
+
+ targets_val = torch.cat([labels for _, labels in tqdm(val_loader, desc="Get Labels")])
+
+ cm = confusion_matrix(targets_val.cpu(), predictions_val.cpu())
+
+ disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=classes)
+
+ # Set figure size
+ plt.figure(figsize=(10, 8))
+
+ disp.plot(cmap=plt.cm.Blues, xticks_rotation="vertical");
+
+ REFER K_FOLD_CROSS_VALIDATION.doc for screen shot
 '''
