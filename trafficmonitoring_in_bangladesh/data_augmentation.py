c21d9c501b1ddac1e687ce4903222481bd7d0e21
raja <rraja48569@gmail.com>
2025-02-04 21:19:54 +0530

    Data augmentation

---
+++ trafficmonitoring_in_bangladesh/data_augmentation.py
@@ -0,0 +1,11 @@
+#                   WHAT IS DATA AUGMENTATION
+'''
+ * There will be an situation when we can't give large amount ot
+   input to build model.
+
+ * It will complicated to memorize all the inputs in model.
+
+ * Here the data augmentation play's it's role.
+
+ * It creates new data based on existing data
+'''
