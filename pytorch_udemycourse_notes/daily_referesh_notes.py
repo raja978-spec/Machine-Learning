eeeff548d3196fa22b4734149dfbb50522f4aff6
raja <rraja48569@gmail.com>
2025-05-15 15:16:01 +0530

    What is pytorch, use of pytorch, features of pytorch

---
+++ pytorch_udemycourse_notes/daily_referesh_notes.py
@@ -0,0 +1,16 @@
+#              USE OF PYTORCH
+'''
+Framework used to create and train neural network model
+
+Used when we need to train a model with image, text
+
+Advatange:
+Dynamic computation graph easy to debug models unlike tensorflow
+Allows tensor operation, tensor is like numpy with more
+feature to graphical processing unit(used to train
+neural network model)
+
+Has various libraries for computer vision(torchvision)
+,NLP(tourch text) and Troruch serve for model deployment
+
+'''
